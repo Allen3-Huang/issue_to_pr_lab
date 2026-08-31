f2717e5fcd29729a6962b1af94527288e88a8301
allen3_huang <allen3_huang@asus.com>
2026-08-31 17:03:25 +0800

    feat: add reports

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+import re
+
 from fastapi import FastAPI
 
-from app.routers import products
+from app.routers import products, reports
 
 app = FastAPI()
 
 app.include_router(products.router)
+app.include_router(reports.router)
+
 
 
 @app.get("/")
